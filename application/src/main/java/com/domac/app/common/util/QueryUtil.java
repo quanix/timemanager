a0d20aad6230b7c5b857d432a95d6790dcea501e
lihaoquan <quanet@163.com>
2014-03-05 16:58:48 +0800

    提交部分工具类

---
+++ application/src/main/java/com/domac/app/common/util/QueryUtil.java
@@ -0,0 +1,73 @@
+package com.domac.app.common.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Author : lihaoquan
+ * Description :
+ */
+public class QueryUtil {
+
+    /**
+     * 判断对象是否Empty(null或元素为0)<br>
+     * 实用于对如下对象做判断:String Collection及其子类 Map及其子类
+     *
+     * @param pObj
+     *            待检查对象
+     * @return boolean 返回的布尔值
+     */
+    public static boolean isEmpty(Object pObj) {
+        if (pObj == null)
+            return true;
+        if (pObj == "")
+            return true;
+        if (pObj instanceof String) {
+            if (((String) pObj).length() == 0) {
+                return true;
+            }
+        } else if (pObj instanceof Collection) {
+            if (((Collection) pObj).size() == 0) {
+                return true;
+            }
+        } else if (pObj instanceof Map) {
+            if (((Map) pObj).size() == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断对象是否为NotEmpty(!null或元素>0)<br>
+     * 实用于对如下对象做判断:String Collection及其子类 Map及其子类
+     *
+     * @param pObj
+     *            待检查对象
+     * @return boolean 返回的布尔值
+     */
+    public static boolean isNotEmpty(Object pObj) {
+        if (pObj == null)
+            return false;
+        if (pObj == "")
+            return false;
+        if (pObj instanceof String) {
+            if (((String) pObj).length() == 0) {
+                return false;
+            }
+            if(((String) pObj).equalsIgnoreCase("null")) {
+                return false;
+            }
+        } else if (pObj instanceof Collection) {
+            if (((Collection) pObj).size() == 0) {
+                return false;
+            }
+        } else if (pObj instanceof Map) {
+            if (((Map) pObj).size() == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
