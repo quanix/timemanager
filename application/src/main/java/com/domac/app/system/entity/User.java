50852bf22c18d747fe1ba97889ebe42acad39bac
quanet <quanet@163.com>
2014-04-12 23:34:08 +0800

    引入Shiro的依赖支持

--- application/src/main/java/com/domac/app/system/entity/User.java
+++ application/src/main/java/com/domac/app/system/entity/User.java
@@ -7,4 +7,20 @@ import com.domac.app.common.entity.AbstractEntity;
  */
 public class User extends AbstractEntity {
 
+    private String loginName;
+    private String name;
+    private String plainPassword;
+    private String password;
+    private String salt;
+    private String roles;
+    private String registerDate;
+
+    public User() {}
+
+    public User(String id) {
+        this.id = id;
+    }
+
+
+
 }
---
+++ application/src/main/java/com/domac/app/system/service/UserRealm.java
@@ -0,0 +1,37 @@
+package com.domac.app.system.service;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author : lihaoquan
+ */
+public class UserRealm extends AuthorizingRealm {
+
+    /**
+     * 授权查询回调函数
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+
+    /**
+     * 验证查询回调函数
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken)
+            throws AuthenticationException {
+        return null;
+    }
+}
