13a99b2665119c8412d8165e1e772eaa5028994e
quanix <quanix@163.com>
2014-05-23 14:12:31 +0800

    增加mail支持(未实现)

---
+++ application/src/main/java/com/domac/app/support/email/MimeMailService.java
@@ -0,0 +1,16 @@
+package com.domac.app.support.email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * created by quanix
+ *
+ * MIME邮件服务类
+ */
+public class MimeMailService {
+
+    private static final String DEFAULT_ENCODING = "utf-8";
+
+    private static Logger logger = LoggerFactory.getLogger(MimeMailService.class);
+}
